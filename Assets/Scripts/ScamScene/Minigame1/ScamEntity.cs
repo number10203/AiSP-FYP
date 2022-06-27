0af85faeba2f2d63ca6f3863e5729f07b2a88d38
Wittez <69459645+Wittez@users.noreply.github.com>
2022-06-27 16:30:49 +0800

    Added animations for entities being whacked
    still need sprites whitespace to be removed WIP

--- Assets/Scripts/ScamScene/Minigame1/ScamEntity.cs
+++ Assets/Scripts/ScamScene/Minigame1/ScamEntity.cs
@@ -10,9 +10,10 @@ public class ScamEntity : MonoBehaviour, IPointerDownHandler
     private Canvas rend;
     private BoxCollider2D inputCollider;
     private Image spriteRenderer;
+    private Animator entityAnimator;
 
-    [HideInInspector]
-    private float timeUntilSpawn;
+    private bool wasHit = false;
+
     [SerializeField]
     [Range(0, 100)]
     private int maximumTimeUntilSpawn;
@@ -35,8 +36,17 @@ public class ScamEntity : MonoBehaviour, IPointerDownHandler
         public float timedie;
     }
 
+    [System.Serializable]
+    public struct OnHitSpriteTypes
+    {
+        public ENTITY_TYPE type;
+        public List<Sprite> onHitSprites;
+    }
+
     [SerializeField]
     private List<EntityInformation> imagesCollection;
+    [SerializeField]
+    private List<OnHitSpriteTypes> onHitSpritesCollection;
     private EntityInformation currentEntity;
 
     public static ScamEntity Instance
@@ -48,6 +58,7 @@ public class ScamEntity : MonoBehaviour, IPointerDownHandler
     {
         inputCollider = this.GetComponent<BoxCollider2D>();
         spriteRenderer = this.GetComponent<Image>();
+        entityAnimator = this.GetComponent<Animator>();
 
         currentEntity = imagesCollection[Random.Range(0, imagesCollection.Count)];
         spriteRenderer.sprite = currentEntity.entitySprite;
@@ -55,15 +66,7 @@ public class ScamEntity : MonoBehaviour, IPointerDownHandler
         thisTransform.sizeDelta = new Vector2(currentEntity.entitySprite.rect.width * 0.02f, currentEntity.entitySprite.rect.height * 0.02f);
         inputCollider.size = new Vector2(thisTransform.rect.width, thisTransform.rect.height);
         rend = this.transform.parent.gameObject.GetComponent<Canvas>();
-        //if (this.gameObject.transform.position.y >= 100)
-        //{
-        //    rend.overrideSorting = true;
-        //    rend.sortingOrder -= 1;
-        //    if (this.gameObject.transform.position.y >= 200)
-        //    {
-        //        rend.sortingOrder -= 1;
-        //    }
-        //}
+
         int spawnPointIndex = spawnPoint.GetSiblingIndex();
         if (spawnPointIndex == 0)
             spawnPointIndex++;
@@ -77,7 +80,9 @@ public class ScamEntity : MonoBehaviour, IPointerDownHandler
 
     private void Update()
     {
-        currentEntity.timealive += Time.deltaTime;
+        if (!wasHit)
+            currentEntity.timealive += Time.deltaTime;
+
         if (currentEntity.timealive >= currentEntity.timedie)
         {
             if (currentEntity.score >= 0)
@@ -101,8 +106,7 @@ public class ScamEntity : MonoBehaviour, IPointerDownHandler
 
     public void OnPointerDown(PointerEventData p)
     {
-        Debug.Log("I GOT WHACKED :(");
-        //entityCanvasGroup.alpha = 0;
+        wasHit = true;
         if (currentEntity.score <= 0)
         {
             if (ScamManager_1.Instance.score != 0)
@@ -114,15 +118,40 @@ public class ScamEntity : MonoBehaviour, IPointerDownHandler
         {
             ScamManager_1.Instance.score += currentEntity.score;
         }
+
+        if (entityAnimator != null)
+        {
+            AssignSpritesForOnHit();
+            entityAnimator.Play("Entity_OnHit");
+        }
+        else
+        {
+            DespawnEntity();
+        }
+    }
+
+    public void AssignSpritesForOnHit()
+    {
+        Image[] images = this.GetComponentsInChildren<Image>(true);
+
+        foreach (OnHitSpriteTypes onHitSprites in onHitSpritesCollection)
+        {
+            if (onHitSprites.type == currentEntity.type)
+            {
+                for (int i = 1; i - 1 < onHitSprites.onHitSprites.Count; i++)
+                {
+                    images[i].sprite = onHitSprites.onHitSprites[i - 1];
+                }
+                break;
+            }
+        }
+    }
+
+    public void DespawnEntity()
+    {
         this.gameObject.SetActive(false);
         spawnPoint.gameObject.SetActive(true);
         ScamSpawner1.Instance.DeleteEntity(this.transform.parent.gameObject);
-        //Destroy(this.gameObject);
-
-        //currpos = this.gameObject.transform.position;
-        //spawnPoint = Instantiate(ScamSpawner1.Instance.spawnPointOrigin, currpos, Quaternion.identity);
-        //ScamSpawner1.Instance.spawnPoints.Add(spawnPoint);
     }
-
 }
  
