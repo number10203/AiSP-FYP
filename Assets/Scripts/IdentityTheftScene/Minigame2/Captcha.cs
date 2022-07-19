09c0e219f8e31d867bc9af6c9f27e6d285e56980
xinjie kho <xinjie.kho@kingmakergames.co>
2022-07-19 08:13:10 +0800

    Implement spawner and slection, add score problem

---
+++ Assets/Scripts/IdentityTheftScene/Minigame2/Captcha.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Captcha : MonoBehaviour
+{
+    public AudioClip correct, wrong;
+
+    private Camera mainCam;
+    private float CameraZDistance;
+
+    public bool isSelected = false;
+    public bool isBad = false;
+
+    private IdentityTheftManager_2 manager;
+
+    public static Captcha Instance
+    {
+        get; private set;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    // Set gameobject child to virus and change tag
+    public void SetToVirus()
+    {
+        //isBad = true;
+        //transform.GetChild(0).gameObject.SetActive(true);
+        //transform.GetChild(1).gameObject.SetActive(false);
+        //tag = "RealVirus";
+    }
+
+    // Set gameobject child to virus and change tag
+    public void SetSelect()
+    {
+        isSelected = !isSelected;
+    }
+
+    // When mouse is up, check if within distance of trashbin object
+    // If within range, check if correct object
+    void OnMouseUp()
+    {
+
+
+
+        //if (Vector3.Distance(transform.position, manager.trashbinPos) <= 2)
+        //{
+        //    GameObject temp = Instantiate(particleEffect, manager.trashbinPos, Quaternion.identity);
+        //    Destroy(temp, 0.5f);
+
+            //    if (gameObject.CompareTag("RealVirus"))
+            //    {
+            //        manager.score += 200;
+            //        manager.correntAns++;
+
+            //        manager.audioManager.Play(correct);
+
+            //        spawner.virusCount--;
+            //        gameObject.SetActive(false);
+            //    }
+            //    else if (gameObject.CompareTag("FakeVirus"))
+            //    {
+            //        if (manager.score > 50)
+            //        {
+            //            manager.score -= 100;
+            //        }
+            //        manager.wrongAns++;
+
+            //        manager.audioManager.Play(wrong);
+
+            //        spawner.virusCount--;
+            //        gameObject.SetActive(false);
+            //    }
+
+            //    if ((manager.correntAns + manager.wrongAns) >= 3)
+            //    {
+            //        if (manager.correntAns > manager.wrongAns)
+            //        {
+            //            manager.isWin = true;
+            //        }
+            //        else
+            //        {
+            //            manager.isLose = true;
+            //        }
+
+            //        //manager.endPanel.SetActive(true);
+            //    }
+            //    else if (spawner.virusCount <= 2)
+            //    {
+            //        spawner.Clear();
+            //        ++manager.qnNumber;
+            //        spawner.Spawn();
+            //    }
+            //    else if (spawner.virusCount > 2)
+            //    {
+            //        spawner.Clear();
+            //    }
+            //}
+    }
+
+}
---
+++ Assets/Scripts/IdentityTheftScene/Minigame2/CaptchaManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CaptchaManager : MonoBehaviour
+{
+    public GameObject captchaPrefab;
+    public Transform captchaOrigin;
+
+    private Canvas rend;
+    private BoxCollider2D inputCollider;
+    private Image spriteRenderer;
+    private Animator entityAnimator;
+    private AudioManager audioManager;
+
+    Toggle m_Toggle;
+    private int selectedObjects;
+
+    private List<GameObject> captchaImage = new List<GameObject>();
+
+
+    private void Start()
+    {
+        SpawnCaptcha();
+    }
+
+    private void Update()
+    {
+        //if (selected == true)
+        //{
+        //    if (addscore == true)
+        //    {
+        //        IdentityTheftManager_2.Instance.score += 100;
+        //        addscore = false;
+        //    }
+        //}
+
+    }
+
+    private void SpawnCaptcha()
+    {
+        // for the columns
+        for (int i = 0; i <= 2; i++)
+        {
+            // for the rows
+            for (int j = 0; j <= 1; j++)
+            {
+                Vector3 pos = new Vector3((i - 1) * 5.25f , (j - 0.7f) * 3.5f);
+                captchaImage.Add(Instantiate(captchaPrefab, pos, Quaternion.identity, captchaOrigin));
+            }
+        }
+
+    }
+
+
+    public void CheckList()
+    {
+        for (int i = 0; i < captchaImage.Count; ++i)
+        {
+            if (!captchaImage[i].GetComponent<Captcha>().isSelected)
+                continue;
+            else
+            {
+                if(!captchaImage[i].GetComponent<Captcha>().isBad)
+                {
+                    IdentityTheftManager_2.Instance.score += 100;
+                    this.gameObject.SetActive(false);
+                    Debug.Log(IdentityTheftManager_2.Instance.score);
+                }
+                
+            }
+        }
+    }
+
+    // Clear all folders
+    public void Clear()
+    {
+        foreach (GameObject go in captchaImage)
+        {
+            Destroy(go);
+        }
+
+        captchaImage.Clear();
+    }
+
+
+
+
+}
