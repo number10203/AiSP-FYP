0e44583f23c8782810fd1683c6b8c3fcf36a5a21
xinjie kho <xinjie.kho@kingmakergames.co>
2022-08-01 16:42:09 +0800

    done animation still need rework cos aabit bug

--- Assets/Scripts/IdentityTheftScene/Minigame2/CaptchaManager.cs
+++ Assets/Scripts/IdentityTheftScene/Minigame2/CaptchaManager.cs
@@ -143,6 +143,8 @@ public class CaptchaManager : MonoBehaviour
                     {
                         IdentityTheftManager_2.Instance.score -= 100;
                         captchaImage[i].GetComponent<Captcha>().hasScored = true;
+                        entityAnimator = captchaImage[i].GetComponent<Animator>();
+                        entityAnimator.Play("Captcha_FlipW");
                     }
                 }
 
@@ -160,7 +162,7 @@ public class CaptchaManager : MonoBehaviour
 
     IEnumerator Clear()
     {
-        yield return new WaitForSeconds(30.0f);
+        yield return new WaitForSeconds(3.0f);
 
         foreach (GameObject go in captchaImage)
         {
